3ff69e6c149d771602eef5c55fb8131d66fc470d
Eman <eman.ashour1111@gmail.co>
2017-04-20 10:55:05 +0200

    feat: Add Authentication to SubjectList
    Add BaseActivity for the Login Logic

--- app/src/main/java/link/ideas/easya/SubjectList.java
+++ app/src/main/java/link/ideas/easya/SubjectList.java
@@ -2,52 +2,36 @@ package link.ideas.easya;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.transition.Fade;
-import android.transition.Slide;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import link.ideas.easya.data.CourseContract;
 import link.ideas.easya.fragment.FavSubjectListFragment;
-import link.ideas.easya.fragment.SubjectDetailFragment;
 import link.ideas.easya.fragment.SubjectListFragment;
 
 
-public class SubjectList extends AppCompatActivity implements SubjectListFragment.Callback, FavSubjectListFragment.Callback {
+public class SubjectList extends BaseActivity implements
+        SubjectListFragment.Callback, FavSubjectListFragment.Callback {
+
     static Uri mUri;
-    private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
-
-    private NavigationView navigationView;
-    private DrawerLayout drawer;
-    private View navHeader;
-    private ImageView imgProfile;
-    private TextView txtName;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +49,7 @@ public class SubjectList extends AppCompatActivity implements SubjectListFragmen
 
 
         }
+        setUpAPIs();
         loadNavHeader();
         setUpNavigationView();
 
@@ -159,94 +144,6 @@ public class SubjectList extends AppCompatActivity implements SubjectListFragmen
         Intent intent = new Intent(this, AddNewLesson.class);
         intent.putExtra("CourseId", CourseContract.SubjectEntry.getSubjectIdFromUri(mUri));
         startActivity(intent);
-    }
-
-    private void loadNavHeader() {
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
-
-        // Navigation view header
-        navHeader = navigationView.getHeaderView(0);
-        txtName = (TextView) navHeader.findViewById(R.id.name);
-        imgProfile = (ImageView) navHeader.findViewById(R.id.img_profile);
-
-    }
-    private void setUpNavigationView() {
-        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-
-            // This method will trigger on item Click of navigation menu
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-                //Check to see which item was being clicked and perform appropriate action
-                switch (menuItem.getItemId()) {
-                    case R.id.nav_home:
-                        Intent intent = new Intent(SubjectList.this, CoursesList.class);
-                        startActivity(intent);
-                        return true;
-                    case R.id.nav_about:
-                        Toast.makeText(SubjectList.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
-                        drawer.closeDrawers();
-                        return true;
-                    case R.id.nav_friends:
-                        Toast.makeText(SubjectList.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
-                        drawer.closeDrawers();
-                        return true;
-                    default:
-                }
-
-                //Checking if the item is in checked state or not, if not make it in checked state
-                if (menuItem.isChecked()) {
-                    menuItem.setChecked(false);
-                } else {
-                    menuItem.setChecked(true);
-                }
-                menuItem.setChecked(true);
-
-
-                return true;
-            }
-        });
-
-
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.drawer_open, R.string.drawer_open) {
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
-                super.onDrawerClosed(drawerView);
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
-                super.onDrawerOpened(drawerView);
-            }
-        };
-
-        //Setting the actionbarToggle to drawer layout
-        drawer.setDrawerListener(actionBarDrawerToggle);
-
-        //calling sync state is necessary or else your hamburger icon wont show up
-        actionBarDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawers();
-            return;
-        }
-
-        super.onBackPressed();
     }
 
 }
